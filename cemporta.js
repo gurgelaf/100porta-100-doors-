0d4490a724d0b5abe8fe9b7c3b3e0847e909c451
Andrezinho <45611521+gurgelaf@users.noreply.github.com>
2020-02-26 20:05:20 -0300

    Create cemporta.js

---
+++ cemporta.js
@@ -0,0 +1,29 @@
+porta = Array(100).fill(false);
+
+function mecherporta (c,porta) {
+  x = porta.length;
+  
+  for (var i = 0; i < x; i = i + c) {
+    porta[i] = !porta[i];
+  }
+
+  if (c < x)
+    return mecherporta(++c, porta);
+  else {
+    porta2 = [], j = 0;
+
+    for (var i = 0; i < x; i++) {
+      if (porta[i] == true){
+        porta2[j++] = i;
+      }
+    }
+
+    return porta2;
+  }
+}
+
+function cemporta() {
+  return mecherporta(1, porta);
+}
+
+console.log(cemporta());
